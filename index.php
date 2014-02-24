c8cdd74628ca65e25e6c55b23f100589d0ff990f
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-02-24 03:28:53 -0600

    new theme

--- index.php
+++ index.php
@@ -1,84 +1,55 @@
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Book Me provides Northwestern students with a quick and easy way to buy and sell textbooks from fellow students and compare prices with online vendors">
+		<meta name="autor" content="Jon Rovira, William Chou, Christina Kim, Divir Gupta">
+		<link rel="shortcut icon" href="images/favicon.ico"> <!--MAKE THIS-->
 
-    <title>Book Me</title>
+		<title>Book Me</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/global.css" rel="stylesheet">
+		<link href="css/index.css" rel="stylesheet">
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	</head>
 
-    <!-- Custom styles for this template -->
-    <link href="css/cover.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+	<body>
+		<div id="wrapper">
+			<div id="container">
 
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+				<!--Navbar-->
+				<div id="navbar" class="clearfix">
+					<div id="navbar-inner">
+						<h3>Book Me</h3>
+						<ul class="nav">
+							<li><a href="#">Buy</a></li>
+							<li><a href="#">Sell</a></li>
+						</ul>
+						<div class="col-sm-6 col-md-8 pull-right">
+							<form class="navbar-form" role="search">
+								<div class="input-group">
+									<input type="text" class="form-control" placeholder="Search by book title, author, or class number" name="srch-term" id="srch-term">
+									<div class="input-group-btn">
+										<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
 
-  <body>
+			</div>
+		</div>
+	</body>
 
-    <div class="site-wrapper">
 
-      <div class="site-wrapper-inner">
-
-        <div class="cover-container">
-
-          <div class="masthead clearfix">
-            <div class="inner">
-              <h3 class="masthead-brand">Book Me</h3>
-              <ul class="nav masthead-nav">
-                <li><a href="#">Buy</a></li>
-                <li><a href="#">Sell</a></li>
-              </ul>
-              <div class="col-sm-6 col-md-8 pull-right">
-                <form class="navbar-form" role="search">
-                  <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
-                    <div class="input-group-btn">
-                      <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-
-          <div class="inner cover">
-            <h1 class="cover-heading">Book Me</h1>
-            <p class="lead">We make buying and selling your textbooks quick and easy.</p>
-            <p class="lead">
-              <a href="#" class="btn btn-lg btn-default">Buy</a>
-              <a href="#" class="btn btn-lg btn-default">Sell</a>
-            </p>
-          </div>
-
-          <div class="mastfoot">
-            <div class="inner">
-              <!--<p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>-->
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-
-    </div>
-
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
 </html>
