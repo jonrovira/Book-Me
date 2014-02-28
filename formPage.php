d7f2c790f55f55590ea554fe59ab6e2e58e18b4b
wtchou712 <wtchou712@gmail.com>
2014-02-28 17:32:04 -0600

    formPage finished

--- formPage.php
+++ formPage.php
@@ -25,7 +25,93 @@
 		<div id="wrapper">
 			<div id="container">
 				<?php include 'header.html'; ?>
-				<?php include 'formPage.html'; ?>
+				<div>
+					<form class = "form-horizontal" role = "form">
+				      <div id="form-top-message">
+				        Complete all fields to post a textbook sale. 
+				      </div>
+
+				      <!--form for inputting the name -->
+				      <div class ="form-group" id="name-form">
+				        <label for ="inputName" class = "col-sm-1 control-label"><font color ="#694489">Name</font></label>
+				        <div class ="col-sm-4">
+				          <input type ="email" class = "form-control" id="inputName" placeholder="Name">
+				        </div>
+				      </div>
+				      <!-- dropdown button for selecting department-->
+				      <div class = "btn-group" id="department-dropdown">
+				        <button type = "button" class = "btn btn-default dropdown-toggle" data-toggle="dropdown"><font color ="#694489">
+				          Department </font><span class = "caret"> </span>
+				        </button>
+				        <ul class = "dropdown-menu" role ="menu">
+				          <li><a href = "#"><font color ="#694489">ANTHRO</font></a></li>
+				          <li><a href = "#"><font color ="#694489">BIO </font></a></li>
+				          <li><a href = "#"><font color ="#694489">BME </font></a></li>
+				          <li><a href = "#"><font color ="#694489">CHEM </font></a></li>
+				          <li><a href = "#"><font color ="#694489">EECS </font></a></li>
+				        </ul>
+				      </div>
+				      <!-- dropdown button for selecting class number --> 
+				      <div class = "btn-group" id="class-number-dropdown">
+				        <button type = "button" class = "btn btn-default dropdown-toggle" data-toggle="dropdown"><font color ="#694489">
+				          Class Number </font><span class = "caret"> </span>
+				        </button>
+				        <ul class = "dropdown-menu" role ="menu">
+				          <li><a href = "#"><font color ="#694489">101</font></a></li>
+				          <li><a href = "#"><font color ="#694489">110 </font></a></li>
+				          <li><a href = "#"><font color ="#694489">202 </font></a></li>
+				          <li><a href = "#"><font color ="#694489">211 </font></a></li>
+				          <li><a href = "#"><font color ="#694489">213 </font></a></li>
+				        </ul>
+				      </div>
+
+				      <!-- form for inputting the book title--> 
+				      <div class ="form-group" id="book-title-form">
+				        <label for ="inputTitle" class = "col-sm-1 control-label"><font color ="#694489">Title</font></label>
+				        <div class ="col-sm-4">
+				          <input type ="email" class = "form-control" id="inputTitle" placeholder="Title">
+				        </div>
+				      </div>
+
+				      <!-- form for inputting the author--> 
+				      <div class ="form-group" id="author-form">
+				        <label for ="inputAuthor" class = "col-sm-1 control-label"><font color ="#694489">Author</font></label>
+				        <div class ="col-sm-4">
+				          <input type ="email" class = "form-control" id="inputAuthor" placeholder="Author">
+				        </div>
+				      </div>
+
+				      <!-- form for inputting the price--> 
+				      <div class ="form-group" id="price-form">
+				        <label for ="inputPrice" class = "col-sm-1 control-label"><font color ="#694489">Price</font></label>
+				        <div class ="col-sm-4">
+				          <input type ="email" class = "form-control" id="inputPrice" placeholder="Price">
+				        </div>
+				      </div>
+
+				      <!-- condition of book dropdown button-->
+				      <div class = "btn-group" id="condition-dropdown">
+				        <button type = "button" class = "btn btn-default dropdown-toggle" data-toggle="dropdown"><font color ="#694489">
+				          Condition </font><span class = "caret"> </span>
+				        </button>
+				        <ul class = "dropdown-menu" role ="menu">
+				          <li><a href = "#"><font color ="#694489">New</font></a></li>
+				          <li><a href = "#"><font color ="#694489">Used - Like New </font></a></li>
+				          <li><a href = "#"><font color ="#694489">Used - Good Condition  </font></a></li>
+				          <li><a href = "#"><font color ="#694489">Used - Poor Condition</font></a></li>
+				        </ul>
+				      </div>
+
+				      <div class ="form-group" id="edition-form">
+				        <label for ="inputEdition" class = "col-sm-1 control-label"><font color ="#694489">Edition</font></label>
+				        <div class ="col-sm-4">
+				          <input type ="email" class = "form-control" id="inputEdition" placeholder="Edition">
+				        </div>
+				      </div>
+
+				      <button type="button" id="submit-button" class="btn btn-default btn-sm">Submit</button>
+
+				</div>
 
 			</div>
 		</div>
