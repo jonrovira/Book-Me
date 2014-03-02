25ca3c0d5707543e53cbe699ba3abaf01374b5c6
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-03-01 20:06:39 -0600

    remove indexHeader

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 			<div class="container">
 					<img src="images/Textbook-Xchange-Logo.png" />
 					<div class="input-group">
-						<input type="text" class="form-control" placeholder="Search by book title, author, or class number" name="srch-term" id="srch-term">
+						<input type="text" class="form-control" placeholder="Search by book title, author, or class #" name="srch-term" id="srch-term">
 						<div class="input-group-btn">
 							<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
 						</div>
