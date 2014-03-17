ece903f67f988d06ddbe7213c77d650bbb2dfd72
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-03-17 00:34:44 -0500

    Everything is responsive except modal

--- searchResults.php
+++ searchResults.php
@@ -27,7 +27,7 @@
 		<div class="my-container">
 			<?php include 'header.html'; ?>
 
-			<nav class="navbar navbar-default" role="navigation">
+			<nav id="filters" class="navbar navbar-default" role="navigation">
 			  	<div class="container-fluid">
 			    	<!-- Brand and toggle get grouped for better mobile display -->
 				    <div class="navbar-header">
